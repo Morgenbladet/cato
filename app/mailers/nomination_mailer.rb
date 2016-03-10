f44715334120ba59cd44952d84b5d7e0a29604f1
Sigve Indregard <sigve@indregard.no>
2016-03-10 14:21:25 +0100

    Fix typo in subject line of email

--- app/mailers/nomination_mailer.rb
+++ app/mailers/nomination_mailer.rb
@@ -1,6 +1,6 @@
 class NominationMailer < ApplicationMailer
   def nomination_verified(nomination)
     @nomination = nomination
-    mail to: @nomination.nominator_email, subject: 'Din nominasjon er gddkjent'
+    mail to: @nomination.nominator_email, subject: 'Din nominasjon er godkjent'
   end
 end
