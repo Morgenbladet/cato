0e20c841a53d70ed4aedb33f45498f2fcb34507a
Sigve Indregard <sigve@indregard.no>
2016-03-10 11:05:17 +0100

    More frontend

--- public/frontend.js
+++ public/frontend.js
@@ -1,4 +1,5 @@
 jQuery(function($) {
+  //const HOSTNAME = 'http://localhost:3000';
   const HOSTNAME = 'https://cato.herokuapp.com';
 
   var root_div = $("#cato_root");
@@ -20,7 +21,10 @@ jQuery(function($) {
     $.get(HOSTNAME + "/institutions.json")
       .done(function(data) {
         $.each(data, function(index, element) {
-          root_div.append(institution_box(element));
+          var box = institution_box(element);
+          root_div.append(box);
+          box.hide();
+          box.delay(index * 100).fadeIn(200);
         });
       })
       .fail(function() {
@@ -47,21 +51,80 @@ jQuery(function($) {
     return(anchor);
   };
 
+  var nomination_li = function(data) {
+    var element = $("<li class='nomination'></li>");
+    element.append("<h3>" + data.name + "</h3>");
+    element.append("<div class='reason'>«" + data.reason + "»</div>");
+    return(element);
+  };
 
   var institution_show = function(id) {
     clear_and_spinner();
+    var back_link = $("<a href='#institutions'>« Alle institusjoner</a>");
+    back_link.click(institutions_index);
+    root_div.append(back_link);
+
+    $.get(HOSTNAME + '/institutions/' + id + ".json")
+      .done(function(data) {
+        root_div.prepend($("<h2>" + data.name + "</h2>"));
+      });
 
     $.get(HOSTNAME + '/nominations.json?institution=' + id)
       .done(function(data) {
-      
+        if(data.length == 0) {
+          root_div.append($("<p>Ingen har nominert noen herfra ennå. Nominér din favoritt!</p>"));
+        } else {
+          var ul = $("<ul class='nomination_list'></ul>");
+          $.each(data, function(index, element) {
+            ul.append(nomination_li(element));
+          });
+          root_div.append(ul);
+        }
       })
       .fail(function() {
-
+        root_div.append($("<p>En feil oppstod under henting av nominasjoner.</p>"));
       })
       .always(function() {
         hide_spinner();
+        var form = submission_form(id);
+        root_div.append(submission_form(id));
+        grecaptcha.render($(".g-recaptcha")[0]);
       });
+
   }
+
+  var input_div = function(field, description) {
+    var div = $("<div class='input_field'></div>");
+    div.append($("<span class='field_text'>" + description + "</span><br/>"));
+    div.append($("<input name='nomination[" + field + "]'/>"));
+    return(div);
+  };
+
+  var submission_form = function(institution_id) {
+    var form = $("<form id='submission'></form>");
+    form.append($("<input name='nomination[institution_id]' type='hidden' value='" + institution_id + "'/>"));
+    form.append(input_div("name", "Navn på den nominerte:"));
+    form.append(input_div("nominator", "Ditt navn:"));
+    form.append(input_div("nominator_email", "Din e-post:"));
+    var ta = $("<div class='input_field'></div>");
+    ta.append($("<span class='field_text'>Begrunnelse:</span><br/>"));
+    ta.append($("<textarea name='nomination[reason]' cols='30' rows='10'></textarea>"));
+    form.append(ta);
+    form.append($("<button type='submit'>Send nominasjon!</button>"));
+
+    form.submit(function(e) {
+      $.post(HOSTNAME + '/nominations.json', $("#submission").serialize())
+        .done(function(data) {
+          alert('Din nominasjon er mottatt. Den blir behandlet manuelt før den vises på denne siden.');
+        })
+        .fail(function(data) {
+          alert('Noe gikk galt med denne nominasjonen. Forsøk gjerne igjen.\n\n' + data.responseText);
+        });
+      e.preventDefault();
+    });
+
+    return(form);
+  };
 
   institutions_index();
 });
