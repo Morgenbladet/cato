8572c20038a726a612171bcb04acafda43bfeef4
Sigve Indregard <sigve@indregard.no>
2016-04-14 12:56:29 +0200

    New frontend (votes)

--- public/frontend.js
+++ public/frontend.js
@@ -18,59 +18,168 @@ jQuery.scrollTo = function (target, offset, speed, container) {
   }, speed);
 };
 
+var has_voted_for = function(id) {
+  return (parseInt(id) in Cookies.getJSON('cato_voted'));
+}
+
+var set_voted_for = function(id) {
+  var votes = Cookies.getJSON('cato_voted');
+  votes[parseInt(id)] = true;
+  Cookies.set('cato_voted', votes);
+}
+
 jQuery(function($) {
   //const HOSTNAME = 'http://localhost:3000';
   const HOSTNAME = 'https://cato.herokuapp.com';
 
-  var root_div = $("#cato_root");
-  var spinner = $("<img src='//cato.herokuapp.com/img/ripple.svg' id='spinner' style:'display:none'/>");
-  root_div.prepend(spinner);
+  var root_div;
+  var spinner;
+
+  var init = function() {
+    root_div = $("#cato_root");
+    filter_div = $("<div class='filter'/>");
+    filter_div.append(filter_select());
+    filter_div.append(" eller søk på navn: ");
+    filter_div.append(filter_text());
+    root_div.before(filter_div);
+
+    var vote_cookie = Cookies.getJSON('cato_voted');
+    if (typeof vote_cookie == 'undefined') {
+      Cookies.set('cato_voted', {})
+    }
+
+    spinner = $("<p><img src='//cato.herokuapp.com/img/ripple.svg' id='spinner' style:'display:none'/></p>");
+    root_div.before(spinner);
+
+    if (/institution=\d+/.test(window.location.hash)) {
+      institution_show(window.location.hash.match(/institution=(\d+)$/)[1]);
+    } else {
+      featured_nominations();
+    }
+  }
+
+  var filter_select = function() {
+    var select = $("<select id='institutions'>");
+    select.append($('<option selected>–– Alle læresteder</option>'));
+
+    select.change(function() {
+      inst = $("#institutions option:selected").data();
+      if (typeof inst.id == 'undefined') {
+        featured_nominations();
+      } else {
+        institution_nominations(inst);
+      }
+    });
+
+    $.get(HOSTNAME + "/institutions.json")
+      .done(function(data) {
+        $.each(data, function(index, element) {
+          select.append($('<option/>', {
+            value: element.id,
+            text:  element.name + " (" + element.nominations_count + ")",
+            data:  element
+          }));
+        });
+      })
+      .fail(function() {
+        select.replaceWith($("<p>Kunne ikke laste institusjonene...</p>")); 
+      });
+
+    return(select);
+  }
+
+  var filter_text = function() {
+    var field = $("<input/>", {
+      id: 'cato_search',
+      type: 'text',
+      placeholder: 'Navn'
+    });
+
+    var button = $('<button/>', {
+      text: 'Søk'
+    });
+
+    button.click(function() {
+      var txt = $("#cato_search").val();
+      if (txt == '') {
+        featured_nominations();
+      } else {
+        search(txt);
+      }
+    });
+
+    var fs = $("<span>");
+    fs.append(field);
+    fs.append(button);
+
+    return(fs);
+  };
+
 
   var clear_and_spinner = function() {
-    root_div.html('');
-    spinner.show();
+    clear();
+    spin();
   }
+
+  var clear = function() { root_div.html(''); }
+
+  var spin = function() { spinner.show(); }
 
   var hide_spinner = function() {
     spinner.hide();
   }
 
-  var institutions_index = function () {
-    clear_and_spinner();
-    window.location.hash = "institutions";
-    $.get(HOSTNAME + "/institutions.json")
+
+  var list_nominations = function (uri) {
+    spin();
+    var ul = $("<ul/>");
+
+    $.get(HOSTNAME + uri)
       .done(function(data) {
-        root_div.append("<p>Klikk på ditt universitet/høyskole for å lese nominasjoner eller bidra med din egen.</p>");
-        $.each(data, function(index, element) {
-          var box = institution_box(element);
-          root_div.append(box);
-          box.hide();
-          box.delay(index * 100).fadeIn(200);
-        });
+        if (data.length) {
+          $.each(data, function(index, element) {
+            var li = nomination_li(element);
+            ul.append(li);
+            li.hide();
+            li.delay(index*200).fadeIn(500);
+          });
+          root_div.append(ul);
+        } else {
+          root_div.append("<p><em>Ingen nominasjoner funnet.</em></p>");
+        }
       })
       .fail(function() {
-        notify_error("Kunne ikke laste studiestedene. Prøv på nytt senere.");
+        root_div.append("<p>Kunne ikke laste nominasjonene…</p>");
       })
       .always(function() {
         hide_spinner();
-        $.scrollTo(root_div, -50);
       });
+  }
+
+  var featured_nominations = function() {
+    clear();
+    root_div.append("<h2>Ti tilfeldig valgte:</h2>");
+    var button = $("<button>Hent ti nye!</button>");
+    button.click(featured_nominations);
+    root_div.append(button);
+    list_nominations('/nominations/random.json');
+  }
+
+  var institution_nominations = function(institution) {
+    clear();
+    root_div.append("<h2>" + institution.name + "</h2>");
+    list_nominations('/nominations.json?institution=' + institution.id);
+  }
+
+  var search = function(txt) {
+    clear();
+    txt = encodeURIComponent(txt);
+    root_div.append("<h2>Søk: " + txt + "</h2>");
+    list_nominations('/nominations.json?search=' + txt);
   }
 
   var notify_error = function(error) {
     root_div.html("En feil oppstod: " + error);
-  };
-
-  var institution_box = function(institution) {
-    var anchor = $("<a href='#'></a>");
-    anchor.data({ action: 'show-institution', id: institution.id });
-    var element = $("<div class='institution'></div>");
-    element.append("<h3>" + institution.name + "</h3>");
-    anchor.append(element);
-    anchor.click(function() {
-      institution_show($(this).data('id'));
-    });
-    return(anchor);
   };
 
   var social_icons = function(data) {
@@ -88,12 +197,20 @@ jQuery(function($) {
     return(div);
   };
 
+
   var vote_button = function(id) {
     button = $("<button class='votebutton'>✔ Støtt denne nominasjonen!</button>");
+
+    if (has_voted_for(id)) {
+      button.prop('disabled', true);
+      button.text('✔ Du har støttet denne!');
+    }
+
     button.click(function(e) {
       $.post(HOSTNAME + '/nominations/' + id + '/vote.json')
         .done(function(data) {
           $(e.currentTarget).prop('disabled', true);
+          set_voted_for(id);
           alert("Din stemme er mottatt!");
         })
         .fail(function(data) {
@@ -105,13 +222,17 @@ jQuery(function($) {
   }
 
   var nomination_li = function(data) {
-    var element = $("<li class='nomination'></li>");
-    element.append("<h3>" + data.name + "</h3>");
-    element.append("<div class='reason'><b>Begrunnelse:</b> " + data.reason_html + "</div>");
-    element.append("<p><i>Nominert av " + data.nominator + "</i></p>");
-    element.append(vote_button(data.id));
-    element.append(social_icons(data));
-    return(element);
+    var li = $("<li class='nomination'></li>");
+    li.append("<h3>" + data.name + "</h3>");
+    $.each(data.reasons, function(index, element) {
+      var div = $("<div class='reason'>");
+      div.append("<blockquote>" + element.reason_html + "</blockquote>");
+      div.append("<p class='nominator'>– " + element.nominator + "</p>");
+      li.append(div);
+    });
+    li.append(vote_button(data.id));
+    li.append(social_icons(data));
+    return(li);
   };
 
   var institution_show = function(id) {
@@ -189,9 +310,7 @@ jQuery(function($) {
     return(form);
   };
 
-  if (/institution=\d+/.test(window.location.hash)) {
-    institution_show(window.location.hash.match(/institution=(\d+)$/)[1]);
-  } else {
-    institutions_index();
-  }
+
+  init();
+
 });
