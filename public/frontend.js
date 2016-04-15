bf39fd4c62dbcaf054939222b2efba608f90f736
Sigve Indregard <sigve@indregard.no>
2016-04-15 11:29:47 +0200

    Improvements to frontend

--- public/frontend.js
+++ public/frontend.js
@@ -29,14 +29,13 @@ var set_voted_for = function(id) {
 }
 
 jQuery(function($) {
-  //const HOSTNAME = 'http://localhost:3000';
-  const HOSTNAME = 'https://cato.herokuapp.com';
-
+  var HOSTNAME;
   var root_div;
   var spinner;
 
   var init = function() {
     root_div = $("#cato_root");
+    HOSTNAME = root_div.data('host') || 'https://cato.herokuapp.com';
     filter_div = $("<div class='filter'/>");
     filter_div.append("Velg et lærested:<br/>");
     filter_div.append(filter_select());
@@ -54,6 +53,8 @@ jQuery(function($) {
 
     if (/institution=\d+/.test(window.location.hash)) {
       institution_show(window.location.hash.match(/institution=(\d+)$/)[1]);
+    } else if(/nomination=\d+/.test(window.location.hash)) {
+      nomination_show(window.location.hash.match(/nomination=(\d+)$/)[1] );
     } else {
       featured_nominations();
     }
@@ -146,9 +147,12 @@ jQuery(function($) {
             li.delay(index*200).fadeIn(500);
           });
           root_div.append(ul);
+        } else if (typeof data.name !== 'undefined') {
+          root_div.append(nomination_li(data));
         } else {
           root_div.append("<p><em>Ingen nominasjoner funnet.</em></p>");
         }
+        root_div.append(random_button('Hent ti tilfeldige'));
       })
       .fail(function() {
         root_div.append("<p>Kunne ikke laste nominasjonene…</p>");
@@ -158,12 +162,16 @@ jQuery(function($) {
       });
   }
 
+  var random_button = function(txt) {
+    var button = $("<button>" + txt + "</button>");
+    button.click(featured_nominations);
+    return(button);
+  }
+
   var featured_nominations = function() {
     clear();
     root_div.append("<h2>Ti tilfeldig valgte:</h2>");
-    var button = $("<button>Hent ti nye!</button>");
-    button.click(featured_nominations);
-    root_div.append(button);
+    root_div.append(random_button('Hent ti nye'));
     list_nominations('/nominations/random.json');
   }
 
@@ -171,6 +179,25 @@ jQuery(function($) {
     clear();
     root_div.append("<h2>" + institution.name + "</h2>");
     list_nominations('/nominations.json?institution=' + institution.id);
+  }
+
+  var institution_show = function(id) {
+    clear();
+    spin();
+    $.get(HOSTNAME + '/institutions/' + id + '.json')
+      .done(function(data) {
+        institution_nominations(data);
+      })
+      .fail(function() {
+        root_div.append("Fant ikke lærestedet...");
+      })
+      .always(function() {
+        hide_spinner();
+      });
+  }
+
+  var nomination_show = function(id) {
+    list_nominations('/nominations/' + id + '.json');
   }
 
   var search = function(txt) {
@@ -189,11 +216,11 @@ jQuery(function($) {
     var ul = $("<ul>");
     var fb = $("<li class='fb'></li>");
     var tw = $("<li class='tw'></li>");
-    fb.append('<a href="https://www.facebook.com/share.php?u=https://morgenbladet.no/fantastiskeformidlere#institution=' + data.institution_id + '" onclick="return mb15_fb_click(this)" target="_blank" title="Del på facebook" class="icon"></a>');
+    fb.append('<a href="https://www.facebook.com/share.php?u=https://morgenbladet.no/fantastiskeformidlere#nomination=' + data.id + '" onclick="return mb15_fb_click(this)" target="_blank" title="Del på facebook" class="icon"></a>');
     ul.append(fb);
     ul.append(tw);
     var string = encodeURIComponent("Nominert til fantastiske formidlere: " + data.name);
-    var url = encodeURIComponent("https://morgenbladet.no/fantastiskeformidlere#institution=" + data.institution_id);
+    var url = encodeURIComponent("https://morgenbladet.no/fantastiskeformidlere#nomination=" + data.id);
     tw.append('<a href="https://twitter.com/intent/tweet?text=' + string + '&amp;url=' + url + '&amp;via=Morgenbladet" onclick="return mb15_tw_click(this)" target="_blank" title="Del på Twitter" class="icon"></a>');
     div.append(ul);
     return(div);
@@ -237,81 +264,6 @@ jQuery(function($) {
     });
     li.append(vote_button(data.id));
     return(li);
-  };
-
-  var institution_show = function(id) {
-    clear_and_spinner();
-    var back_link = $("<a href='#institutions'>« Andre institusjoner</a>");
-    back_link.click(institutions_index);
-    root_div.append(back_link);
-
-    $.get(HOSTNAME + '/institutions/' + id + ".json")
-      .done(function(data) {
-        root_div.prepend($("<h2>" + data.name + "</h2>"));
-      });
-
-    $.get(HOSTNAME + '/nominations.json?institution=' + id)
-      .done(function(data) {
-        if(data.length == 0) {
-          root_div.append($("<p>Ingen har nominert noen herfra ennå. Nominér din favoritt!</p>"));
-        } else {
-          var ul = $("<ul class='nomination_list'></ul>");
-          $.each(data, function(index, element) {
-            ul.append(nomination_li(element));
-          });
-          root_div.append(ul);
-        }
-      })
-      .fail(function() {
-        root_div.append($("<p>En feil oppstod under henting av nominasjoner.</p>"));
-      })
-      .always(function() {
-        hide_spinner();
-        var form = submission_form(id);
-        root_div.append("<h2>Nominér din favoritt</h2>");
-        root_div.append(submission_form(id));
-        window.location.hash = "institution=" + id;
-        $.scrollTo(root_div, -50);
-      });
-
-  }
-
-  var input_div = function(field, description, type) {
-    if (type == 'undefined')
-      type = 'text';
-    var div = $("<div class='input_field'></div>");
-    var fld_name = "nomination["+field +"]";
-    div.append($("<label for='" + fld_name + "'>" + description + "</label><br/>"));
-    div.append($("<input name='" + fld_name + "' type='" + type + "'/>"));
-    return(div);
-  };
-
-  var submission_form = function(institution_id) {
-    var form = $("<form id='submission'></form>");
-    form.append("<p>Studenter, tidligere studenter, kolleger, tilfeldige publikummere – alle kan nominere. På nettsiden kan du også lese andres nominasjoner, stemme frem kandidater og dele nominasjonene videre.</p><p>Nominasjonene og stemmene danner grunnlaget for juryens videre utvelgelse, og begrunnelsen er derfor viktig. Prøv å gi en presis beskrivelse av undervisningen på maksimalt 2000 tegn. Frist for å nominere er 15. april. Juryens liste med ti navn presenteres i august.</p>");
-    form.append($("<input name='nomination[institution_id]' type='hidden' value='" + institution_id + "'/>"));
-    form.append(input_div("name", "Navn på den nominerte:"));
-    form.append(input_div("nominator", "Ditt navn:"));
-    form.append(input_div("nominator_email", "Din e-post:", 'email'));
-    var ta = $("<div class='input_field'></div>");
-    ta.append($("<label for='nomination[reason]'>Begrunnelse:</label><br/>"));
-    ta.append($("<textarea name='nomination[reason]' cols='30' rows='10'></textarea>"));
-    form.append(ta);
-    form.append($("<button type='submit'>Send nominasjon!</button>"));
-
-    form.submit(function(e) {
-      $.post(HOSTNAME + '/nominations.json', $("#submission").serialize())
-        .done(function(data) {
-          alert('Din nominasjon er mottatt. Den blir behandlet manuelt før den vises på denne siden.');
-          institutions_index();
-        })
-        .fail(function(data) {
-          alert('Noe gikk galt med denne nominasjonen. Forsøk gjerne igjen.\n\n' + data.responseText);
-        });
-      e.preventDefault();
-    });
-
-    return(form);
   };
 
 
